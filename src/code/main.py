79fbc6d4d5c1eb515f62043fa7c8d14f8a7bdff8
Benfey <anton.benfey@gmail.com>
2023-06-19 17:13:06 -0300

    stuff

--- src/code/main.py
+++ src/code/main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextBrowser, QLineEdit, QFileDialog, QProgressBar, QComboBox
 from PyQt5.QtCore import QUrl, Qt
 from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QTabWidget, QTextBrowser
 import feedparser
 import sys
 
@@ -8,12 +9,17 @@ class RSSReader(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.tab_widget = QTabWidget()  # Initialize without a parent
+        self.tabs = {}  # We will store our tabs in this dictionary
+        self.all_tab = QTextBrowser()
+        self.tab_widget.addTab(self.all_tab, "All")
+
         self.url_entry = QComboBox(self)
         self.url_entry.setEditable(True)
         self.load_url_button = QPushButton('Load RSS from URL', self)
         self.refresh_button = QPushButton('Refresh', self)
         self.load_file_button = QPushButton('Load RSS from File', self)
-        self.text_browser = QTextBrowser(self)
+        # self.text_browser = QTextBrowser(self)
         self.progress_bar = QProgressBar(self)
         self.progress_bar.setRange(0, 0)
         self.progress_bar.hide()
@@ -23,13 +29,13 @@ class RSSReader(QWidget):
         self.layout.addWidget(self.load_url_button)
         self.layout.addWidget(self.refresh_button)
         self.layout.addWidget(self.load_file_button)
-        self.layout.addWidget(self.text_browser)
+        self.layout.addWidget(self.tab_widget)
         self.layout.addWidget(self.progress_bar)
 
         self.load_url_button.clicked.connect(self.load_rss_from_url)
         self.refresh_button.clicked.connect(self.refresh_rss)
         self.load_file_button.clicked.connect(self.load_rss_from_file)
-        self.text_browser.anchorClicked.connect(QDesktopServices.openUrl)
+        # self.text_browser.anchorClicked.connect(QDesktopServices.openUrl)
 
         self.setStyleSheet("""
             QWidget {
@@ -74,20 +80,32 @@ class RSSReader(QWidget):
         if file_path:
             with open(file_path, 'r') as file:
                 lines = file.readlines()
-                content = ""
                 for line in lines:
-                    content += self.parse_rss(line.strip())
-                self.text_browser.setHtml(content)
+                    content = self.parse_rss(line.strip())
+                    rss_url = line.strip()
+                    if rss_url not in self.tabs:
+                        # If the tab doesn't exist, create it
+                        self.tabs[rss_url] = QTextBrowser()
+                        self.tab_widget.addTab(self.tabs[rss_url], rss_url.split('/')[-1])  # Use the last part of the URL as the tab name
+                    self.tabs[rss_url].setHtml(content)
+                    self.all_tab.append(content)  # Add the content to the 'All' tab
+
 
     def load_rss_from_url(self):
         rss_url = self.url_entry.currentText()
         if rss_url:
             self.progress_bar.show()
             content = self.parse_rss(rss_url)
-            self.text_browser.setHtml(content)
+            if rss_url not in self.tabs:
+                # If the tab doesn't exist, create it
+                self.tabs[rss_url] = QTextBrowser()
+                self.tab_widget.addTab(self.tabs[rss_url], rss_url.split('/')[-1])  # Use the last part of the URL as the tab name
+            self.tabs[rss_url].setHtml(content)
+            self.all_tab.append(content)  # Add the content to the 'All' tab
             self.progress_bar.hide()
             if self.url_entry.findText(rss_url) == -1:
                 self.url_entry.addItem(rss_url)
+
 
     def refresh_rss(self):
         self.load_rss_from_url()
